c139b2a0dc57ea4bdb9062ed8b7a5eedd5b01fe0
Vighnesh Chaudhari <Gamingclash1979@gmail.com>
2026-06-07 12:49:33 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
+const http = require('http');
+
+http.createServer((req, res) => {
+    res.writeHead(200);
+    res.end('Bot is running!');
+}).listen(process.env.PORT || 3000);
+
 const {
     Client,
     GatewayIntentBits,
